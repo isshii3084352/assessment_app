7df707aef03b081a9d39673570d00055ef1d856e
isshii <isshii3084352@gmail.com>
2017-07-25 18:07:01 +0900

    Datum model

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'assessment#home'
   get  '/example',   to: 'assessment#example'
   get  '/formpage',  to: 'assessment#formpage'
+  post '/formpage',  to: 'assessment#sendinfo'
   get  '/result',    to: 'assessment#result'
 end
